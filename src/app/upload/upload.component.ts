7bebcadb3228579ea695fd6111e13e1c1af49140
RahulSolanki <rahulsolankib@gmail.com>
2019-10-30 00:16:19 +0530

    deletion and downloading image works

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -12,7 +12,8 @@ import * as firebase from 'firebase';
 export class UploadComponent implements OnInit {
   file;
   url="";
-  
+  name=""; 
+  delname=""; 
   private uploadTask: firebase.storage.UploadTask;
   constructor(private Pic:UploadImageService,private firestore:AngularFirestore) { }
 
@@ -26,24 +27,39 @@ export class UploadComponent implements OnInit {
   {
     var file=this.file;
     
-    console.log(file)
+   // console.log(file)
     
     var storage = firebase.storage();
   
     // Create a storage reference from our storage service
-    var storageRef = storage.ref('name/'+file.name);
+    var storageRef = storage.ref('farm/'+file.name);
     //this.Pic.uploadImage(file);
     storageRef.put(file);
   }
   downloadUrl()
   {
     var url1="";
+    //console.log('/farm/'+this.name+'.jpg')
     var storage = firebase.storage();
-    var starsRef = storage.ref('/name/taj.jpg');
+    var starsRef = storage.ref(`/farm/${this.name}.jpg`);
     starsRef.getDownloadURL().then((url)=>{
       this.url=url;
-      console.log(this.url)
+    //  console.log(this.url)
+    }).catch(function(error) {
+        confirm(error.code_)
     })
+  }
+  deleteImage()
+  {
+    // Delete the file
+    if(confirm("Are you sure? you want to delete image "+this.delname+".jpg")){
+        var desertRef = firebase.storage().ref(`farm/${this.delname}.jpg`)
 
+        desertRef.delete().then(function() {      
+          confirm("Deleted successfully");
+        }).catch(function(error) {
+          confirm("Uh-oh, an error occurred!");
+        });
+      }
   }
 }
